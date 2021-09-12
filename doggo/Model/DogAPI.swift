157d055a67c3ad091ec40fe56fabdd19f8714e25
Jameka Echols <jameka.echols@gmail.com>
2021-09-11 23:22:59 -0500

    added enum for endpoint and added computed property

---
+++ doggo/Model/DogAPI.swift
@@ -0,0 +1,21 @@
+//
+//  DogAPI.swift
+//  doggo
+//
+//  Created by Jameka Echols on 9/11/21.
+//
+
+import Foundation
+
+class DogAPI {
+    enum Endpoint: String {
+        case randomImageFromAllDogsCollection = "https://dog.ceo/api/breeds/image/random"
+        
+        
+        // computed property
+        var url : URL {
+            return URL(string: self.rawValue)! // never force unwrap but this is a guaranteed working url
+        }
+    }
+}
+
